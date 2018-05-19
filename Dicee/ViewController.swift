958afc3132f940415345d20107887e87a1f93edb
Tohid Fahim <tohidfahim3110@gmail.com>
2018-05-19 13:34:50 +0600

    App 1

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Md. Tohidur Rahman on 16/5/18.
+//  Copyright © 2018 Md. Tohidur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButttonPressed(_ sender: UIButton) {
+    
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        //print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    //motion is froom help -> developer documentation
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
